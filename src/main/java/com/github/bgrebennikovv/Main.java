43c4404da4a58882b3b0a1d585fc74ef4fb078d9
Boris <bgrebennikovv@gmail.com>
2024-12-15 20:21:50 +0400

    Task 1 - Define client OS

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -3,5 +3,14 @@ package com.github.bgrebennikovv;
 public class Main {
     public static void main(String[] args) {
 
+
+        // Task 1
+        int clientOS = 1; // 0 - IOS 1 - Android
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else{
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
     }
 }
