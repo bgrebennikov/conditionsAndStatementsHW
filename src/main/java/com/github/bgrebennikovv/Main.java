67972fb0c8474e888536ceec3dba453986a2fa3e
Boris <bgrebennikovv@gmail.com>
2024-12-15 20:35:19 +0400

    extract clientOS values to class

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -1,6 +1,6 @@
 package com.github.bgrebennikovv;
 
-final class OS{
+final class ClientOS {
     public static final int IOS = 0;
     public static final int ANDROID = 1;
 }
@@ -11,12 +11,13 @@ public class Main {
 
 
         // Task 1
-        int clientOS = OS.IOS; // Это для удобства, сути не меняет. Можно легко ошибиться если использовать только числовые значения!
+        int clientOS = ClientOS.IOS; // Это для удобства, сути не меняет. Можно легко ошибиться если использовать только числовые значения!
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
 
 
 
