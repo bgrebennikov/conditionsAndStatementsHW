570fe8c6520f1871ae77b253a0bda9803867b5fd
Boris <bgrebennikovv@gmail.com>
2024-12-15 21:03:36 +0400

    Task 5 - seasons number

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -71,5 +71,43 @@ public class Main {
                 Потребуется дней: %s
                 """, deliveryDistance, deliveryDays);
 
+
+        // Task 5
+        int monthNumber = 12;
+
+        String season;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                season = "зима";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "весна";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "лето";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "осень";
+                break;
+            default:
+                season = null;
+                break;
+        }
+
+        if (season != null) {
+            System.out.println("Месяц номер " + monthNumber + " принадлежит к сезону: " + season + ".");
+        } else {
+            System.out.println("Некорректный номер месяца. Введите значение от 1 до 12.");
+        }
+
+
     }
 }
